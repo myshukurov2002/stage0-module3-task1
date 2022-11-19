f6a7cb64af3958dab6565100044cdcc6db816090
myshukurov2002 <107299390+myshukurov2002@users.noreply.github.com>
2022-11-19 23:52:39 +0500

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-    System.out.println("PrintSentenceApp");
+    System.out.println("This is my first Java program");
     }
 }
